5a444a5f6625eb355bf7c481aec23c00ca242a2c
Yago Leal <yagoleal.php@gmail.com>
2014-07-23 17:10:23 +0700

    'add menu entity'

--- src/Menu.php
+++ src/Menu.php
@@ -6,10 +6,6 @@ use ZeroPHP\ZeroPHP\Entity;
 class Menu extends Entity {
 
     function __construct() {
-        // Get Regions of Theme_default and Theme_admin
-        $regions = \Config::get('theme.regions', array());
-        asort($regions);
-
         $this->setStructure(array(
             'id' => 'block_id',
             'name' => 'block',
@@ -25,38 +21,25 @@ class Menu extends Entity {
                     'title' => zerophp_lang('Title'),
                     'type' => 'input',
                 ),
-                'cache_type' => array(
+                'cache' => array(
                     'name' => 'cache_type',
-                    'title' => zerophp_lang('Cache type'),
-                    'type' => 'dropdown_build',
-                    'options' => array(
-                        'full' => zerophp_lang('Full cache'),
-                        'page' => zerophp_lang('Page cache'),
-                    ),
+                    'title' => zerophp_lang('Cache'),
+                    'type' => 'input',
+                ),
+                'path' => array(
+                    'name' => 'path',
+                    'title' => zerophp_lang('Path'),
+                    'type' => 'input',
                     'validate' => 'required',
                 ),
-                'region' => array(
-                    'name' => 'region',
-                    'title' => zerophp_lang('Region'),
-                    'type' => 'dropdown_build',
-                    'options' => $regions,
-                    'validate' => 'required',
-                ),
-                'content' => array(
-                    'name' => 'content',
-                    'title' => zerophp_lang('Content'),
-                    'type' => 'textarea',
-                    'rte_enable' => 1,
-                    'display_hidden' => 1,
-                ),
-                'library' => array(
-                    'name' => 'library',
+                'class' => array(
+                    'name' => 'class',
                     'title' => zerophp_lang('Class'),
                     'type' => 'input',
                     'display_hidden' => 1,
                 ),
-                'function' => array(
-                    'name' => 'function',
+                'method' => array(
+                    'name' => 'method',
                     'title' => zerophp_lang('Method'),
                     'type' => 'input',
                     'display_hidden' => 1,
@@ -94,62 +77,9 @@ class Menu extends Entity {
             return $cache;
         }
 
-        $blocks_raw = parent::loadEntityAll();
+        $menus = parent::loadEntityAll();
 
-        $blocks = array();
-        if (count($blocks_raw)) {
-            foreach ($blocks_raw as $key => $block) {
-                $blocks[$block->region][$block->block_id] = $block;
-            }
-        }
-
-        \Cache::forever(__METHOD__, $blocks);
-        return $blocks;
-    }
-
-    public function run($block) {
-        if (self::_checkBlocksVisible($block)) {
-            if ($block->cache_type) {
-                $cache_name = __METHOD__ . $block->block_id;
-                switch ($block->cache_type) {
-                    case 'full':
-                        $cache_name .= "full";
-                        break;
-
-                    case 'page':
-                        $cache_name .= "page-" . md5(\URL::current());
-                        break;
-                }
-
-                $cache = \Cache::get($cache_name);
-                if ($cache) {
-                    return $cache;
-                }
-            }
-
-            $class = new $block->library;
-            $method = $block->function;
-            $block_content = $class->$method($block);
-            //$block_content = 'Hello AK';
-
-            if ($block->cache_type) {
-                \Cache::put($cache_name, $block_content, ZEROPHP_CACHE_EXPIRE_TIME);
-            }
-
-            return $block_content;
-        }
-
-        return '';
-    }
-
-    private function _checkBlocksVisible($block) {
-        if (empty($block->access)) {
-            return true;
-        }
-
-        $class = new $block->library;
-        $method = $block->access;
-
-        return $class->$method($block);
+        \Cache::forever(__METHOD__, $menus);
+        return $menus;
     }
 }
