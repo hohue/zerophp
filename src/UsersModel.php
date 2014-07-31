6d248093969dd8816205c47a35f0bb47a26a7a50
Yago Leal <yagoleal.php@gmail.com>
2014-07-31 11:19:36 +0700

    user change pass initial

--- src/UsersModel.php
+++ src/UsersModel.php
@@ -3,29 +3,29 @@ namespace ZeroPHP\ZeroPHP;
 
 class UsersModel {
 
-    function user_id_get($email) {
-        $this->db->select('user_id');
+    function id_get($email) {
+        $this->db->select('id');
         $this->db->where('email', $email);
         $query = $this->db->get('users');
 
         if ($query->num_rows() > 0) {
             foreach ($query->result() as $row) {
-                return $row->user_id;
+                return $row->id;
             }
         }
 
         return 0;
     }
 
-    function activation_get_user_id($activation_id) {
-        $this->db->select('user_id');
+    function activation_get_id($activation_id) {
+        $this->db->select('id');
         $this->db->where('activation_id', $activation_id);
         $this->db->where('field', 'activation');
         $query = $this->db->get('users_activation');
 
         if ($query->num_rows() > 0) {
             foreach ($query->result() as $row) {
-                return $row->user_id;
+                return $row->id;
             }
         }
 
