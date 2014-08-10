f5b65e63e1475dad1cc3ead8c435a0e41bd9045c
Yago Leal <yagoleal.php@gmail.com>
2014-08-10 21:51:18 +0700

    Remove split code.

--- solr/src/Solr.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ZeroPHP\Solr;
-
-class Solr {
-    function searchResultEmpty() {
-        $vars = array();
-        $zerophp->response->addContent(zerophp_view('search_search_no_result', $vars));
-    }
-}
