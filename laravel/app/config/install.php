073f2332e4133cb013edc4fac3cec86b7d47d3d2
Yago Leal <yagoleal.php@gmail.com>
2014-08-11 08:52:53 +0700

    add menu

---
+++ laravel/app/config/install.php
@@ -0,0 +1,16 @@
+<?php
+//@todo 7 Move to DB
+return array(
+	'prev_version_zerophp_zerophp' => 0,
+	
+	'prev_version_zerophp_shop' => 0,
+    'prev_version_zerophp_profile' => 0,
+    'prev_version_zerophp_article' => 0,
+    'prev_version_zerophp_category' => 0,
+    'prev_version_zerophp_contact' => 0,
+    'prev_version_zerophp_metadata' => 0,
+    'prev_version_zerophp_location' => 0,
+
+	'prev_version_inetco_chovip' => 0,
+    'prev_version_inetco_esms' => 0,
+);
